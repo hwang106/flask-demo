4b2427f13b402eb257c6f6f8099841917e844e0b
Huan Wang <huan.w.wang@gmail.com>
2021-06-14 01:13:47 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+# allows us to send html file as one hard-coded unit
+from flask import render_template
+#allows us to access form data
+from flask import request, redirect
+from model import get_breakfast_rating
+from datetime import datetime
 
 
 # -- Initialization section --
@@ -13,4 +17,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html", time=datetime.now())
+    #don't need path since render_template method always looks for the file in the templates folder by default
+
+@app.route('/secret')
+def secret():
+    return "You found the secret page!"
+# Had to add methods parameter and include POST (GET is default) so that post method is allowed.
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'POST':
+        print(request.form["breakfast"])
+        user_breakfast = request.form["breakfast"]
+        user_nickname = request.form["nickname"]
+        breakfast_rating = get_breakfast_rating(user_breakfast)
+        return render_template("results.html", bleh=user_breakfast, meh=user_nickname, rating=breakfast_rating)
+    else:
+        return redirect('/')
